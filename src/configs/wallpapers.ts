783cad06427f26921b99632a220803139f740573
tvkvijaythecm <tvkvijaythecm@gmail.com>
2026-05-18 04:21:28 +0800

    Update wallpapers.ts

--- src/configs/wallpapers.ts
+++ src/configs/wallpapers.ts
@@ -1,8 +1,8 @@
 import type { WallpaperData } from "~/types";
 
 const wallpapers: WallpaperData = {
-  day: "img/ui/suresh.png",
-  night: "img/ui/suresh.png"
+  day: "img/ui/file_00000000d0b872089982a152e675999a.png",
+  night: "img/ui/file_00000000d0b872089982a152e675999a.png"
 };
 
 export default wallpapers;
